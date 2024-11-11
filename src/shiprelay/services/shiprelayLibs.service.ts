4cf70e1370527471381391e8274f78a7d5106957
jatinkumar <jatin.kumar@legistify.com>
2024-11-11 15:46:28 +0530

    retry implementation

--- src/shiprelay/services/shiprelayLibs.service.ts
+++ src/shiprelay/services/shiprelayLibs.service.ts
@@ -11,131 +11,150 @@ import { CacheStore } from '@squareboat/nest-cache';
 export class ShipRelayLibService {
     constructor(private readonly httpService: HttpService, private config: ConfigService) { }
 
-    async login(): Promise<string | boolean> {
-        const store = CacheStore("redis");
-        const tokenKey = 'shipRelayToken';
-
-        let isLoginTokenExist = await store.get(tokenKey);
-        if (isLoginTokenExist) {
-            return isLoginTokenExist;
-        } else {
-            let email = this.config.get('services.shipRelay.shipRelayUserName');
-            let password = this.config.get('services.shipRelay.shipRelayPassWord');
-            const response = await lastValueFrom(
-                this.httpService.post(`${process.env.SHIPRELAY_API_URL}/login`, { email, password }).pipe(
-                    map(response => response.data),
-                    catchError((error: AxiosError) => {
-                        throw new BadRequestException(error);
-                    })
-                ),
-            );
-
-            const accessToken = response?.access_token;
-            if (accessToken) {
-                await store.set(tokenKey, accessToken, 31536000); // Set with a TTL of 1 year (31,536,000 seconds)
-                return accessToken;
+    private async retryRequestWithNewToken(requestFn: () => Promise<any>, retries = 3): Promise<any> {
+        for (let attempt = 1; attempt <= retries; attempt++) {
+            console.log({ type: 'INFO', message: `Attempting attempt - ${attempt}...` });
+            try {
+                return await requestFn();
+            } catch (error) {
+                console.log({ type: 'ERROR-INFO', attempt, message: `Attempt ${attempt} failed, retrying...` });
+                if (error instanceof UnauthorizedException && attempt < retries) {
+                    await this.login();
+                } else {
+                    throw error;
+                }
             }
-
-            return false;
         }
     }
 
-    async fetchProductLibService(reqBody: FetchProductDto): Promise<any> {
-        const token = await this.login();
+    private async getToken(): Promise<string> {
+        const store = CacheStore("redis");
+        const tokenKey = 'shipRelayToken';
+        let token = await store.get(tokenKey);
 
+        if (!token) {
+            token = await this.login();
+            if (token) {
+                await store.set(tokenKey, token, 31536000); // Set token with TTL of 1 year
+            }
+        }
+        return token;
+    }
+
+    async login(): Promise<string | boolean> {
+        let email = this.config.get('services.shipRelay.shipRelayUserName');
+        let password = this.config.get('services.shipRelay.shipRelayPassWord');
         const response = await lastValueFrom(
-            this.httpService.get(`${process.env.SHIPRELAY_API_URL}/products?page=${reqBody.page}&per_page=${reqBody.limit}`, {
-                headers: { Authorization: `Bearer ${token}` }
-            }).pipe(
+            this.httpService.post(`${process.env.SHIPRELAY_API_URL}/login`, { email, password }).pipe(
                 map(response => response.data),
                 catchError((error: AxiosError) => {
-                    if (error.response.status == 401)
-                        throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
-                    else
-                        throw new BadRequestException(error);
+                    throw new BadRequestException(error);
                 })
-            )
-        )
+            ),
+        );
 
-        return { data: response.data, meta: response.meta }
+        return response?.access_token || null;
+    }
+
+    async fetchProductLibService(reqBody: FetchProductDto): Promise<any> {
+        return this.retryRequestWithNewToken(async () => {
+            const token = await this.getToken();
+            const response = await lastValueFrom(
+                this.httpService.get(`${process.env.SHIPRELAY_API_URL}/products?page=${reqBody.page}&per_page=${reqBody.limit}`, {
+                    headers: { Authorization: `Bearer ${token}` }
+                }).pipe(
+                    map(response => response.data),
+                    catchError((error: AxiosError) => {
+                        if (error.response.status == 401)
+                            throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
+                        else
+                            throw new BadRequestException(error);
+                    })
+                )
+            );
+            return response?.data?.length ? { data: response.data, meta: response.meta } : [];
+        });
     }
 
     async fetchProductInfoLibService(productId: string): Promise<any> {
-        const token = await this.login();
+        return this.retryRequestWithNewToken(async () => {
+            const token = await this.getToken();
 
-        return await lastValueFrom(
-            this.httpService.get(`${process.env.SHIPRELAY_API_URL}/products/${productId}`, {
-                headers: { Authorization: `Bearer ${token}` }
-            }).pipe(
-                map(response => response.data),
-                catchError((error: AxiosError) => {
-                    console.log('error: ', error.response);
-                    if (error.response.status == 401)
-                        throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
-                    else if (error.response.status == 404)
-                        throw new NotFoundException(__('shipRelay.productNotFoundError'))
-                    else
-                        throw new BadRequestException(error);
-                })
+            return await lastValueFrom(
+                this.httpService.get(`${process.env.SHIPRELAY_API_URL}/products/${productId}`, {
+                    headers: { Authorization: `Bearer ${token}` }
+                }).pipe(
+                    map(response => response.data),
+                    catchError((error: AxiosError) => {
+                        console.log('error: ', error.response);
+                        if (error.response.status == 401)
+                            throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
+                        else if (error.response.status == 404)
+                            throw new NotFoundException(__('shipRelay.productNotFoundError'))
+                        else
+                            throw new BadRequestException(error);
+                    })
+                )
             )
-        )
+        });
     }
 
     async productCreationLibService(reqBody: ProductCreationDto): Promise<any> {
-        const token = await this.login();
+        return this.retryRequestWithNewToken(async () => {
+            const token = await this.getToken();
 
-        return await lastValueFrom(
-            this.httpService.post(`${process.env.SHIPRELAY_API_URL}/products/simple`, reqBody, {
-                headers: { Authorization: `Bearer ${token}` }
-            }).pipe(
-                map(response => response.data),
-                catchError((error: AxiosError) => {
-                    if (error.response.status == 401)
-                        throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
-                    else
-                        throw new BadRequestException(error.response.data);
-                })
+            return await lastValueFrom(
+                this.httpService.post(`${process.env.SHIPRELAY_API_URL}/products/simple`, reqBody, {
+                    headers: { Authorization: `Bearer ${token}` }
+                }).pipe(
+                    map(response => response.data),
+                    catchError((error: AxiosError) => {
+                        if (error.response.status == 401)
+                            throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
+                        else
+                            throw new BadRequestException(error.response.data);
+                    })
+                )
             )
-        )
+        })
     }
 
     async productArchiveLibService(productId: string): Promise<any> {
-        const token = await this.login();
-
-        let updatedProductId = +productId;
-
-        return await lastValueFrom(
-            this.httpService.patch(`${process.env.SHIPRELAY_API_URL}/products/${updatedProductId}/archive`, {}, {
-                headers: { Authorization: `Bearer ${token}` },
-            }).pipe(
-                map(response => response.data),
-                catchError((error: AxiosError) => {
-                    console.log('error: ', error);
-                    if (error.response.status == 401)
-                        throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
-                    else
-                        throw new BadRequestException(error.response.data);
-                })
-            )
-        )
+        return this.retryRequestWithNewToken(async () => {
+            const token = await this.getToken();
+            return await lastValueFrom(
+                this.httpService.patch(`${process.env.SHIPRELAY_API_URL}/products/${productId}/archive`, {}, {
+                    headers: { Authorization: `Bearer ${token}` }
+                }).pipe(
+                    map(response => response.data),
+                    catchError((error: AxiosError) => {
+                        if (error.response.status == 401)
+                            throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
+                        else
+                            throw new BadRequestException(error.response.data);
+                    })
+                )
+            );
+        });
     }
 
     async productRestoreLibService(productId: string): Promise<any> {
-        const token = await this.login();
+        return this.retryRequestWithNewToken(async () => {
+            const token = await this.getToken();
 
-        return await lastValueFrom(
-            this.httpService.patch(`${process.env.SHIPRELAY_API_URL}/products/${productId}/restore`, {}, {
-                headers: { Authorization: `Bearer ${token}` }
-            }).pipe(
-                map(response => response.data),
-                catchError((error: AxiosError) => {
-                    console.log('error: ', error);
-                    if (error.response.status == 401)
-                        throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
-                    else
-                        throw new BadRequestException(error.response.data);
-                })
+            return await lastValueFrom(
+                this.httpService.patch(`${process.env.SHIPRELAY_API_URL}/products/${productId}/restore`, {}, {
+                    headers: { Authorization: `Bearer ${token}` }
+                }).pipe(
+                    map(response => response.data),
+                    catchError((error: AxiosError) => {
+                        if (error.response.status == 401)
+                            throw new UnauthorizedException(__('shipRelay.unAuthorizedError'));
+                        else
+                            throw new BadRequestException(error.response.data);
+                    })
+                )
             )
-        )
+        })
     }
 }
